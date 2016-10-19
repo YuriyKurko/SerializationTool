5b56c553bad566d10ff413368392221bea856edc
YuriyKurko <yuriy.kurko@gmail.com>
2016-10-19 06:27:53 +0300

    Finish Project

--- SerializationTool/MainWindow.xaml.cs
+++ SerializationTool/MainWindow.xaml.cs
@@ -1,28 +1,104 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+using winForms = System.Windows.Forms;
+using System.IO;
+using MahApps.Metro.Controls;
+using Microsoft.Win32;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SerializationTool
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        void browseFolder()
+        {
+            List<string> filesList = new List<string>();
+            winForms.FolderBrowserDialog selectDirectory = new winForms.FolderBrowserDialog();
+
+
+            if (selectDirectory.ShowDialog() == winForms.DialogResult.OK)
+            {
+                var allFiles = from file in
+                               Directory.GetFiles(selectDirectory.SelectedPath, "*.*", SearchOption.AllDirectories)
+                               select file;
+
+                var allDirectories = from file in
+                                Directory.GetDirectories(selectDirectory.SelectedPath, "*.*", SearchOption.AllDirectories)
+                                     select file;
+
+                filesList = allFiles.ToList();
+
+                foreach (var directory in allDirectories)
+                {
+                    if (!Directory.EnumerateFiles(directory).Any())
+                    {
+                        filesList.Add(directory + @"\");
+                    }
+                }
+
+                SerializableFile.Serialize(filesList);
+
+                statusLabel.Content = "Serialized";
+                statusLabel.Foreground = Brushes.DeepSkyBlue;
+            }
+            else
+            {
+
+                return;
+            }
+        }
+
+        void openFile()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+
+            openFileDialog.Filter = "Binary file (*.bin)|*.bin";
+            openFileDialog.Multiselect = false;
+            if (openFileDialog.ShowDialog() == true)
+            {
+                SerializableFile.Deserialize(openFileDialog.FileName);
+
+                statusLabel.Content = "Deserialized";
+                statusLabel.Foreground = Brushes.SkyBlue;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void openFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            statusLabel.Content = "";
+
+            if (switcher.IsChecked == true)
+            {
+                browseFolder();
+            }
+            else
+            {
+                openFile();
+            }
+        }
+
+        private void switcher_Click(object sender, RoutedEventArgs e)
+        {
+            if (switcher.IsChecked == true)
+            {
+                openFileButton.Content = "Select folder";
+            }
+            else
+            {
+                openFileButton.Content = "Open file";
+            }
         }
     }
 }
---
+++ SerializationTool/SerializableFile.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Windows;
+using winForms = System.Windows.Forms;
+
+namespace SerializationTool
+{
+    [Serializable]
+    class SerializableFile
+    {
+        private string directoryPath { get; set; }
+        private string fileName { get; set; }
+        private byte[] fileData { get; set; }
+
+        public SerializableFile(string directoryPath, string fileName, byte[] fileData)
+        {
+            this.directoryPath = directoryPath;
+            this.fileName = fileName;
+            this.fileData = fileData;
+        }
+
+        public static void Serialize(List<string> filesPathList)
+        {
+            FileInfo fileInfo;
+            byte[] tempFile;
+            List<SerializableFile> serializableFileList = new List<SerializableFile>();
+
+            foreach (var filePath in filesPathList)
+            {
+                fileInfo = new FileInfo(filePath);
+                string directoryPath = fileInfo.DirectoryName;
+                string directoryRoot = fileInfo.Directory.Root.ToString();
+                try
+                {
+                    if (Directory.EnumerateFileSystemEntries(directoryPath).Any())
+                    {
+                        tempFile = File.ReadAllBytes(filePath);
+                    }
+                    else
+                    {
+                        tempFile = null;
+                    }
+
+                    serializableFileList.Add(new SerializableFile(fileInfo.DirectoryName.Replace(directoryRoot, ""), fileInfo.Name, tempFile));
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return;
+                }
+
+            }
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Binary file (*.bin)|*.bin";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                FileStream serializedData = new FileStream(saveFileDialog.FileName, FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
+
+                BinaryFormatter binFormatter = new BinaryFormatter();
+
+                binFormatter.Serialize(serializedData, serializableFileList);
+
+                serializedData.Close();
+
+                Process.Start(Path.GetDirectoryName(saveFileDialog.FileName));
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        public static void Deserialize(string filePath)
+        {
+            List<SerializableFile> serializableFileList = new List<SerializableFile>();
+            FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+            BinaryFormatter binFormatter = new BinaryFormatter();
+            winForms.FolderBrowserDialog selectDirectory = new winForms.FolderBrowserDialog();
+            string selectedDirectory;
+
+            if (selectDirectory.ShowDialog() == winForms.DialogResult.OK)
+            {
+                selectedDirectory = selectDirectory.SelectedPath + @"\";
+            }
+            else
+            {
+                return;
+            }
+
+            try
+            {
+                serializableFileList = (List<SerializableFile>)binFormatter.Deserialize(fileStream);
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+
+            fileStream.Close();
+
+            foreach (SerializableFile data in serializableFileList)
+            {
+                string newDirectoryPath = selectedDirectory + data.directoryPath;
+
+                if (!Directory.Exists(newDirectoryPath))
+                {
+                    Directory.CreateDirectory(newDirectoryPath);
+                    if(data.fileData != null)
+                    {
+                        File.WriteAllBytes(newDirectoryPath + @"\" + data.fileName, data.fileData);
+                    }
+                }
+            }
+
+            Process.Start(selectedDirectory);
+        }
+    }
+}
